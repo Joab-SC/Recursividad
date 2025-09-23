048b8d3b2b9843e89074614ff79861d1fb9b6c2d
LauraDuarte720 <laurac.duarteq@uqvirtual.edu.co>
2025-09-22 23:56:55 -0500

    feat: ejercicio 5 al 8

--- main.exs
+++ main.exs
@@ -2,14 +2,23 @@ defmodule Main do
 
 
   def main do
-    factorial(4)
-    |> IO.puts()
+    # factorial(4)
+    # |> IO.puts()
 
-    IO.puts("#{contar_lista([1,2,3,4,5])}")
+    # IO.puts("#{contar_lista([1,2,3,4,5])}")
 
-    IO.puts("#{balance_final([1,2,3,-4,5])}")
+    # IO.puts("#{balance_final([1,2,3,-4,5])}")
 
-    IO.puts("#{elemento_presente?(-4, [2,3,-4,2])}")
+    # IO.puts("#{elemento_presente?(-4, [2,3,-4,2])}")
+
+    # IO.puts("#{calcular_dias_reservados([%{cliente: "Laura", dias: 3}, %{cliente: "Joab", dias: 7}, %{cliente: "Sharif", dias: 4}])}")
+
+    # IO.puts("#{potencia_numero(2,3)}")
+
+    # IO.puts("#{sumar_pares([1,6,9,4,8,2,7])}")
+
+    IO.puts("#{calcular_cantidad_pedidos([%{producto: "Leche", cantidad: 3}, %{producto: "Tomate", cantidad: 7}, %{producto: "Lechuga", cantidad: 5}])}")
+
 
   end
 
@@ -69,11 +78,53 @@ defmodule Main do
     end
 
 
+  # Quinto ejercico
 
-
+  def calcular_dias_reservados([])do
+    0
   end
 
 
+  def calcular_dias_reservados([hd | tl]) do
+    Map.get(hd,:dias) + calcular_dias_reservados(tl)
+  end
+
+  # Sexto ejercicio
+
+  def potencia_numero(_,0) do
+    1
+  end
+
+  def potencia_numero(n,exponente) do
+    n * potencia_numero(n, exponente - 1)
+  end
+
+  # Septimo ejercicio
+
+  def sumar_pares([]) do
+    0
+  end
+
+  def sumar_pares([hd|tl]) when rem(hd,2) == 0 do
+    hd + sumar_pares(tl)
+  end
+
+  def sumar_pares([_|tl]) do
+    0 + sumar_pares(tl)
+  end
+
+  # Septimo ejercicio
+
+  def calcular_cantidad_pedidos([])do
+    0
+  end
+
+
+  def calcular_cantidad_pedidos([hd | tl]) do
+    Map.get(hd,:cantidad) + calcular_cantidad_pedidos(tl)
+  end
+
+  end
 
 
 Main.main()
